14378c41d6cec459bd859e33dfc07fc6f9d52fe8
Anupam Panwar <anupam.panwar@asu.edu>
2016-03-27 18:27:45 -0700

    yaml to json dump

---
+++ Code/Batch_conversion.py
@@ -0,0 +1,44 @@
+import yaml
+import json
+import argparse
+import os
+import datetime
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--folder',
+                        action='store',
+                        required='true',
+                        help='Path to the directory of yaml files to parse')
+    return parser
+
+def jdefault(value):
+    if isinstance(value, datetime.date):
+        return dict(year=value.year, month=value.month, day=value.day)
+    else:
+        return value.__dict__
+
+def main():
+    parser = parse_arguments()
+    args = parser.parse_args()
+    folder_path = args.folder
+
+
+    for f in os.listdir(folder_path):
+        python_path = os.path.join(folder_path, f)
+        with open(python_path, 'r') as stream:
+            try:
+                data = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        with open('data.txt', 'w') as outfile:
+            jsondata =json.dumps(data, outfile, ensure_ascii=False, default=jdefault).encode('utf-8')
+        print(jsondata)
+
+##  For MAC users - Add argument like this -f "Data/odis"
+##  For Window users - Add argument like this -f "Data\\odis`"
+
+
+if __name__ == '__main__':
+    main()
+
