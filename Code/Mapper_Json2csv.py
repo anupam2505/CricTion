651903bd25c884125a7698e530a1617a758ba232
Anupam Panwar <anupam.panwar@asu.edu>
2016-03-29 14:34:02 -0700

    CSV script bug fixing

--- Code/Mapper_Json2csv.py
+++ Code/Mapper_Json2csv.py
@@ -15,6 +15,7 @@ def parse_arguments():
                         action='store',
                         required='true',
                         help='Path to the directory where we want to store csv')
+    return parser
 
 def main():
     parser = parse_arguments()
@@ -25,24 +26,32 @@ def main():
 
     for f in os.listdir(folder_path):
         file_path = os.path.join(folder_path, f)
-        with open(file_path, "w") as f:
-            data = json.loads(f)
+
+        with open(file_path) as jsonf:
+            data = json.load(jsonf)
             file_name = ntpath.basename(str(file_path)).split('.')[0]
-            destf = csv.writer(open("%s/%s.csv" % dest_folder_path % file_name, "w", "wb+"))
+            destf = csv.writer(open("%s/%s.csv" % (dest_folder_path , file_name), "wb+"))
             run =0;
             wickets =0;
-            destf.writerow(["Date","Venue","Balls", "Runs", "Wickets", "Toss", "DN","RPO", "Team1", "Team2", "Team_Won"])
+            destf.writerow(["Date","Venue","Balls", "Runs", "Wickets", "Toss", "Team_Playing","RPO", "Team1", "Team2", "Team_Won"])
             for i in data['innings'][0]["1st innings"]["deliveries"]:
                 day = data["meta"]["created"]["day"]
                 month= data["meta"]["created"]["month"]
                 year = data["meta"]["created"]["year"]
-                date =datetime.datetime.strptime(day+month+year, "%d%m%Y").date()
+                date =datetime.datetime.strptime(str(day)+str(month)+str(year), "%d%m%Y").date()
                 venue = data["info"]["city"]
-                ballkey = i.keys()[0]
                 ballstr = i.keys()[0].split('.')
                 ball = ballstr[0]*6 + ballstr[1]
                 run = run + i[i.keys()[0]]["runs"]["total"]
-
+                if 'wicket' in i.keys():
+                    wickets = wickets+1
+                toss = data["info"]["toss"]["winner"]
+                team_playing = data['innings'][0]["1st innings"]["team"]
+                RPO = int(run*6)/int(ball)
+                team1 = data['innings'][0]["1st innings"]["team"]
+                team2 = data['innings'][1]["2nd innings"]["team"]
+                team_won = data["info"]["outcome"]["winner"]
+                destf.writerow([date, venue, ball, run, wickets, toss, team_playing, RPO, team1, team2, team_won] )
 
 
 if __name__ =='__main__':
