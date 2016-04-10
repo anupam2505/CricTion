43cbd6d0939f26fc3d3a48e6d74b92af7140412e
Anupam Panwar <anupam.panwar@asu.edu>
2016-04-09 23:24:31 -0700

    SVM added

--- Code/Model.py
+++ Code/Model.py
@@ -1,6 +1,7 @@
 import statsmodels.api as sm
-from sklearn import cross_validation
+from sklearn import cross_validation as cv
 from sklearn import svm
+from sklearn import metrics
 
 def logistic(df):
     train_cols = df.columns[1:]
@@ -8,9 +9,15 @@ def logistic(df):
     result = logit.fit()
     print df.describe()
 
-def cross_validation(df):
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split(df, df["Won"], test_size=0.1, random_state=0)
-    clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-    clf.score(X_test, y_test)
+def SVM_cross_validation(df):
+    clf = svm.SVC(kernel='linear', C=1)
+    scores = cv.cross_val_score(clf, df,df['Won'] , cv=2)
+    print scores
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    predicted = cv.cross_val_predict(clf, df,df["Won"], cv=10)
+    print metrics.accuracy_score(df["Won"], predicted)
+    # X_train, X_test, y_train, y_test = cv.train_test_split(df, df["Won"], test_size=0.1, random_state=0)
+    # clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+    # print clf.score(X_test, y_test)
 
 
--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from Model import logistic
-from Model import cross_validation
+from Model import SVM_cross_validation
 
 
 def main():
     df = pd.read_csv('temp_afterFeatureEngineering.csv')
     #logistic(df)
-    cross_validation(df)
+    SVM_cross_validation(df)
 
 
 if __name__ =='__main__':
