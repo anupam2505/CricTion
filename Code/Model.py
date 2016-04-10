b94b37eb4c41f693d1ee79eedb3ea97524289530
Anupam Panwar <anupam.panwar@asu.edu>
2016-04-10 01:17:17 -0700

    Model file updated

--- Code/Model.py
+++ Code/Model.py
@@ -1,7 +1,6 @@
 import statsmodels.api as sm
 from sklearn import cross_validation as cv
 from sklearn import svm
-from sklearn import metrics
 
 def logistic(df):
     train_cols = df.columns[1:]
@@ -16,6 +15,8 @@ def SVM_cross_validation(df):
     scores = cv.cross_val_score(clf, df,df_target , cv=2)
     print scores
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
     # predicted = cv.cross_val_predict(clf, df,df["Won"], cv=10)
     # print metrics.accuracy_score(df["Won"], predicted)
     # X_train, X_test, y_train, y_test = cv.train_test_split(df, df["Won"], test_size=0.1, random_state=0)
